3381d4c242c7812cbbad4a6c8877c597a8bcd3e2
Joshua T Corbin <joshua@uber.com>
2015-09-21 19:00:01 -0700

    Update publish for the New World ™

--- publish.sh
+++ publish.sh
@@ -20,52 +20,19 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-# WHAT DO:
-# - bumps last numerical componet in package.json
-# - commits package.json
-# - updates the node dev branch
-# - tags the node dev branch
-#
-# should / assumed to be ran on master branch
-#
-# WHAT NEXT:
-# - verify
-# - git push origin master dev_node --tags
-# - git checkout dev_node && npm publish
-
 set -e
 set -x
-
-cd -L $(dirname -- $(cd -L -- $(dirname -- $0); pwd))
-
-DEV_BRANCH=dev_hyperbahn
 
 if [ "$1" == "" ]; then
     echo "must pass in version as first arg";
     exit 1;
 fi
 
-VERSION=$1
-NPM_TAG=$2
-node >hyperbahn/package.json.new <<EOF
-var data = require("./hyperbahn/package");
-data.version = "$VERSION";
-console.log(JSON.stringify(data, null, 2));
-EOF
+npm version "$1"
 
-mv hyperbahn/package.json.new hyperbahn/package.json
-
-tag=hyperbahn-v$VERSION
-
-git commit hyperbahn/package.json -m "Cut $tag"
-
-localrev=$(env HOME= XDG_CONFIG_HOME= git subtree split --prefix=hyperbahn)
-git update-ref "refs/heads/$DEV_BRANCH" "$localrev" -m "Cut $tag"
-
-git tag -a -m "Tag $tag" "$tag" "$DEV_BRANCH"
 git push origin master --tags
-git push -f origin dev_hyperbahn
 
-git archive --prefix=package/ --format tgz dev_hyperbahn >package.tgz
+git archive --prefix=package/ --format tgz master >package.tgz
 ${NPM:-npm} publish --registry=https://registry.npmjs.org/ package.tgz --tag "${NPM_TAG:-latest}"
 rm package.tgz
+npm cache clean hyperbahn
