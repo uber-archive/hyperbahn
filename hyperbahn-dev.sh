2b25b09581e2199544e50b5a7ad7d0f41c33722b
Andrew de Andrade <aandrade@uber.com>
2015-10-08 18:05:32 -0700

    Improved hyperbahn-dev script with tcurl hostfile setup

--- hyperbahn-dev.sh
+++ hyperbahn-dev.sh
@@ -1,14 +1,24 @@
 #!/bin/sh 
 set -e
 
+H0="127.0.0.1:21300"
+H1="127.0.0.1:21301"
+
+if [ ! -d ./node_modules ]; then
+  npm install
+fi
+
+./setup-dotfiles.sh $H0 $H1
+
 tmux new-session -s 'hyperbahn' -n 'hyperbahn-0' \; \
     new-window -n 'hyperbahn-1' \; \
     new-window -n 'hyperbahn-adhoc' \; \
-    select-window -t:0 \; \
+    select-window -t hyperbahn-0 \; \
     send-keys 'make run-local-0' Enter \; \
-    select-window -t:1 \; \
+    select-window -t hyperbahn-1 \; \
     send-keys 'make run-local-1' Enter \; \
-    select-window -t:2 \; \
-    send-keys 'sleep 1; tcurl -p 127.0.0.1:21300 autobahn health_v1' Enter \; \
-    send-keys 'tcurl -p 127.0.0.1:21301 autobahn health_v1' Enter \; \
+    select-window -t hyperbahn-adhoc \; \
+    send-keys "sleep 1; tcurl -p $H0 autobahn health_v1" Enter \; \
+    send-keys "tcurl -p $H1 autobahn health_v1" Enter \; \
+    send-keys "tcurl autobahn health_v1" Enter \; \
     attach
