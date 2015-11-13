46f7fffe6ce8e1705fe2b67a251072f4c7bb43ae
Raynos <raynos2@gmail.com>
2015-11-12 16:39:53 -0800

    add blacklist() to hyperbahn thrift definition

--- hyperbahn.thrift
+++ hyperbahn.thrift
@@ -8,6 +8,21 @@ exception InvalidServiceName {
     2: required string serviceName
 }
 
+exception InvalidInstanceHostPort {
+    1: required string message
+    2: required string instanceHostPort
+}
+
+exception NotAffineForServiceName {
+    1: required string message
+    2: required string serviceName
+}
+
+exception InvalidTimeToBan {
+    1: required string message
+    2: required i32 timeToBan
+}
+
 struct DiscoveryQuery {
     1: required string serviceName
 }
@@ -25,11 +40,35 @@ struct DiscoveryResult {
     1: required list<ServicePeer> peers
 }
 
+struct BlackListQuery {
+    1: required string serviceName
+    2: required string instanceHostPort
+    3: required i32 timeToBan
+}
+
+struct BlackListResult {
+    1: required bool wasConnected
+}
+
 service Hyperbahn {
     DiscoveryResult discover(
         1: required DiscoveryQuery query
     ) throws (
         1: NoPeersAvailable noPeersAvailable
         2: InvalidServiceName invalidServiceName
+    )
+
+    /*  blacklist() only operates locally.
+
+        It must be called on an affinity node and fanout
+        is out of band for now.
+    */
+    BlackListResult blacklist(
+        1: required BlackListQuery query
+    ) throws (
+        1: InvalidServiceName invalidServiceName
+        2: InvalidInstanceHostPort invalidInstanceHostPort
+        3: NotAffineForServiceName notAffineForServiceName
+        4: InvalidTimeToBan invalidTimeToBan
     )
 }
