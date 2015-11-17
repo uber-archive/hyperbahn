0ffe690c3a9d89170c5670497dc2765dc77e32c8
Joshua T Corbin <joshua@uber.com>
2015-11-17 13:44:43 -0800

    test/slog: add symbolicate_test_cluster_ports.sh

---
+++ test/slog/symbolicate_test_cluster_ports.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+file=$1
+[ -n "$file" ]
+
+prog="sed $file -e '/# TEST SETUP/d' $(
+  grep '^# TEST SETUP:' "$file" |
+  cut -d ':' -f2- |
+  awk "{print \"-e 's/\" \$2 \"/\" \$1 \"/g'\"}" |
+  tr '\n' ' '
+)"
+# echo "$prog" >&2
+eval "$prog"
