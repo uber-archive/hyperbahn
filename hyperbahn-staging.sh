82c4b57b84f2c1cc89f1831d86a326670984a229
Raynos <raynos2@gmail.com>
2015-12-23 10:30:35 -0800

    add staging command

---
+++ hyperbahn-staging.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PUBLIC_IP="$(shell ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')"
+
+H0="$PUBLIC_IP:21300"
+H1="$PUBLIC_IP:21301"
+H2="$PUBLIC_IP:21302"
+H3="$PUBLIC_IP:21303"
+H4="$PUBLIC_IP:21304"
+
+tmux new-session -s 'hyperbahn' -n 'hyperbahn-0' \; \
+    new-window -n 'hyperbahn-1' \; \
+    new-window -n 'hyperbahn-2' \; \
+    new-window -n 'hyperbahn-3' \; \
+    new-window -n 'hyperbahn-4' \; \
+    new-window -n 'hyperbahn-adhoc' \; \
+    send -t hyperbahn-0 'make run-staging-0' Enter \; \
+    send -t hyperbahn-1 'make run-staging-1' Enter \; \
+    send -t hyperbahn-2 'make run-staging-2' Enter \; \
+    send -t hyperbahn-3 'make run-staging-3' Enter \; \
+    send -t hyperbahn-4 'make run-staging-4' Enter \; \
+    select-window -t hyperbahn-adhoc \; \
+    send "tcurl -p $H0 autobahn health_v1" Enter \; \
+    send "tcurl -p $H4 autobahn health_v1" Enter \;
